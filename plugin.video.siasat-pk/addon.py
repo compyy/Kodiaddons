97966958175a31a948795064d2d092c2c1f18d76
compyy <yasirz_cm@yahoo.com>
2016-04-26 15:49:40 +0200

    UPDATE

--- plugin.video.siasat-pk/addon.py
+++ plugin.video.siasat-pk/addon.py
@@ -2,14 +2,28 @@ import xbmc, xbmcgui, xbmcplugin, xbmcaddon
 import urllib2,urllib, re, os, traceback, cgi
 import requests
 
-
-
 Posturl="http://www.siasat.pk/forum/showthread.php?"
 Nexturl="http://www.siasat.pk/forum/"
 DTSurl="http://www.siasat.pk/forum/forumdisplay.php?29-Daily-Talk-Shows/"
 DVurl="http://www.siasat.pk/forum/forumdisplay.php?21-Siasi-Videos/"
 SCurl="http://www.siasat.pk/forum/forumdisplay.php?37-Sports-Corner/"
-Auto_Play=True
+
+
+__addon__       = xbmcaddon.Addon()
+__addonname__   = __addon__.getAddonInfo('name')
+__icon__        = __addon__.getAddonInfo('icon')
+addon_id = 'plugin.video.siasat-pk'
+selfAddon = xbmcaddon.Addon(id=addon_id)
+profile_path =  xbmc.translatePath(selfAddon.getAddonInfo('profile'))
+
+addonPath = xbmcaddon.Addon().getAddonInfo("path")
+addonversion =xbmcaddon.Addon().getAddonInfo("version")
+
+if not selfAddon.getSetting( "dummy" )=="true":
+    selfAddon.setSetting( "dummy" ,"true")
+	
+def ShowSettings(Fromurl):
+	selfAddon.openSettings()
 
 def get_params():
 	param=[]
@@ -35,28 +49,26 @@ def Addtypes():
 	addDir('Daily Talk Shows' ,'DTShows' ,2,'')
 	addDir('Daily Vidoes' ,'DVidoes' ,2,'')
 	addDir('Sports Corner' ,'SCorner' ,2,'')
+	addDir('Settings' ,'Settings' ,99,'',isItFolder=False)
 
 	return
 
 
-def addDir(name,url,mode,iconimage,isItFolder=True,Context=False):
+def addDir(name,url,mode,iconimage,showContext=False,isItFolder=True,linkType=None):
 	u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
 	ok=True
-
 	liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
 	liz.setInfo( type="Video", infoLabels={ "Title": name } )
+	if showContext==True:
+		cmd1 = "XBMC.RunPlugin(%s&linkType=%s)" % (u,"DailyMotion")
+		cmd2 = "XBMC.RunPlugin(%s&linkType=%s)" % (u,"Youtube")
+		liz.addContextMenuItems([('Play DailyMotion video',cmd1),('Play Youtube video',cmd2)],replaceItems=True)
 	
-	if Context==True:
-		cmd1="DailyMotion Link not Available"
-		cmd2="Youtube Link not Available"
-		cmd1 = "PlayShowLink(%s,%s)" % ("DailyMotion",url)
-		cmd2 = "PlayShowLink(%s,%s)" % ("Youtube",url)
-		liz.addContextMenuItems([("Play DailyMotion",cmd1),("Play Youtube",cmd2)])
+	if linkType:
+		u="XBMC.RunPlugin(%s&linkType=%s)" % (u, linkType)
 		
-
 	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=isItFolder)
 	return ok
-
 
 def AddEnteries(name, type=None):
 	if type=='DTShows':
@@ -83,15 +95,14 @@ def AddShows(Fromurl):
 	
 	for i in IMG_list:
 		IMG.append(re.sub("amp;", "",i))
-
-
+		
 	for urls, desc, img in zip(URL,Desc,IMG):
-		addDir(desc, urls, 3, img, True)
+		addDir(desc, urls, 3, img, showContext=True, isItFolder=True)
 
 	match =re.findall('<span class="prev_next"><a rel="next" href="(.*)["]\stitle="Next Page', link, re.IGNORECASE)
 
 	if len(match)==2:
-		addDir('Next Page' ,match[0] ,2,'', True)
+		addDir('Next Page' ,match[0] ,2,'', isItFolder=True)
 
 	return
 
@@ -102,23 +113,39 @@ def convert(s):
         return s.group(0)
 
 def GetShowLink(url):
+	global linkType
 	headers = {'User-Agent' : 'Mozilla 5.10'}
 	request=urllib2.Request(url, None, headers)
 	response=urllib2.urlopen(request)
 	link=response.read()
 	did=re.findall('<iframe.*src=["]http.*dailymotion.com.*video[/](.*)[?].*["]',link)
 	yid=re.findall('<iframe.*YouTube.*src=["].*youtube[.]com.*[/](.*)[?].*["].*iframe>',link)
-
-	if did:
-		#addDir("DailyMotion", did[0], 4, '', isItFolder=False)
-		PlayShowLink("DailyMotion", did[0])
-		
+	APlay=selfAddon.getSetting("APlay")
 	
-	elif yid:
-		#addDir("Youtube", yid[0], 4, '', isItFolder=False)
-		PlayShowLink("Youtube", yid[0])
+	if APlay=="true":
+		if did:
+			PlayShowLink("DailyMotion", did[0])
+		
+		elif yid:
+			PlayShowLink("Youtube", yid[0])
+		
+		return
+	
+	if linkType:
+			if linkType=="DailyMotion":
+				if did:
+					PlayShowLink("DailyMotion", did[0])
+				else:
+					xbmcgui.Dialog().ok(__addonname__,"No DailyMotion link found")
 
+			if linkType=="Youtube":
+				if yid:
+					PlayShowLink("Youtube", yid[0])
+				else:
+					xbmcgui.Dialog().ok(__addonname__,"YouTube link found")
+	
 	return
+
 
 def PlayShowLink(name,url):
 	if name=="DailyMotion":
@@ -134,6 +161,7 @@ params=get_params()
 url=None
 name=None
 mode=None
+linkType=None
 
 try:
 	url=urllib.unquote_plus(params["url"])
@@ -149,9 +177,15 @@ except:
 	pass
 
 args = cgi.parse_qs(sys.argv[2][1:])
+linkType=''
+
+try:
+	linkType=args.get('linkType', '')[0]
+except:
+	pass
 
 
-print 	mode,url,name
+print 	mode,url,name,linkType
 
 try:
 	if mode==None or url==None or len(url)<1:
@@ -163,8 +197,8 @@ try:
 	elif mode==3:
 		GetShowLink(Posturl+url)
 
-	#elif mode==4:
-		#PlayShowLink(name,url)
+	elif mode==99 :
+		ShowSettings(url)
 
 except:
 	print 'Something dint work'
